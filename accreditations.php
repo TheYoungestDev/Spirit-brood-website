4ee8bed83b90cb063438cd1db07d6b4593bc0b7b
Abdullahi Tijani <iamabdullahitijani@gmail.com>
2024-11-15 18:11:05 +0100

    tired

--- accreditations.php
+++ accreditations.php
@@ -17,70 +17,58 @@
             <div class="container">
                 <div class="row">
                 <div class="col-md-12">
-                        <h2 class="featured-heading-text1">Accreditation</h2><img src="/images/c/AICCS.png" alt="aciis" />
-                        <h3 class="featured-heading-text2">Certified Member of AICCS <br>
-                            Association of Independent Christian Colleges and Seminaries</h3>
-                        <p class="featured-heading-textp">Association of Independent Christian Colleges and Seminaries (AICCS) is a professional, faithbased, non-profit, educational corporation chartered in the State of Missouri to found, develop,
-                            strengthen, and accredit Christian schools of ministry which offer in-classroom and on-line
-                            courses, tracks, and degrees.                            
+                                            <!-- <h2 class="featured-heading-text1">Accreditation Page </h2>  -->
+                                            <br><br>
+                        <!-- <h2 class="featured-heading-text1">Accreditation</h2><img src="/images/c/AICCS.png" alt="aciis" /> -->
+                        <!-- <h3 class="featured-heading-text2">Certified Member of AICCS <br>
+                            Association of Independent Christian Colleges and Seminaries</h3> -->
+                        <p class="featured-heading-textp">Spirit Brood Theological Seminary operates under the legal framework provided by the State of
+                            Florida, where we are duly incorporated as a Religious Institution. Florida law permits us to offer
+                                educational programs exclusively designed to prepare students for religious vocations, as
+                                  <b> ministers, professionals, or laypersons </b>  in the areas of ministry, counseling, theology, education,
+                                        administration, music, fine arts, media communications, and social work.                          
                         </p>
-                        <p class="featured-heading-textp">Based on our status with AICCS, about a thousand institutions worldwide accept students with
-                            credits and/or degrees from Spirit Brood. Some USDE accredited schools also accept our
-                            credentials. You can check our status with AICCS, as well as other AICCS schools that accept our credentials
-                            <a href="https://www.aiccs.org/updates" style="color: rgb(38, 119, 226); text-decoration: underline;" target="_blank">here</a>. Note, however, transference of credit cannot be guaranteed as all credit transfers are at
-                            the discretion of the accepting institution.       
+                        <p class="featured-heading-textp">We fully comply with the requirements of <b>Section 1005.06(1)(f), Florida Statutes,</b> and <b>Rule 6E5.001, Florida Administrative Code.</b> Consequently, we are not subject to the jurisdiction or
+                        purview of the Florida Commission for Independent Education and are not required to obtain
+                            licensure. Additionally, all degree programs offered by Spirit Brood Theological Seminary adhere
+                                to the commission’s duration standards, and our consumer practices align with the provisions of
+                                   <b> Section 1005.04, Florida Statutes. </b> 
                         </p>
                         <br>
-                        <h2 class="featured-heading-text2" style="font-weight: 500;">U.S. Department of Education Accreditation</h2>
-                        <p class="featured-heading-textp">USDE accreditation is not necessary for the theological degrees which we offer.
-                                Generally speaking, people working in ministry positions do not need a USDE
-                                accredited degree. However, individuals seeking employment in governmentlicensed positions such as public school teachers, state- licensed psychologists
-                                or psychiatrists, and non-church-related counselors will more than likely need
-                                USDE accredited degrees.
+                       
+                        <p class="featured-heading-textp">In accordance with Florida law for religious exemption from licensure/accreditation, Spirit Brood
+                            Theological Seminary has filed and submitted a notarized <b>Form 113RE</b> to the Commission for
+                                Independent Education as required.
                             </p>
                             <p class="featured-heading-textp">If you are pursuing education at Spirit Brood Theological Seminary with the
                                         intent of obtaining employment or for some other reason, you should check
                                         with that organization BEFORE applying for enrollment. Spirit Brood
                                         Theological Seminary assumes no liability of any kind.</p>
+                        <br><br>
+                        <h2 class="featured-heading-text2" style="font-weight: 500;"><b>Membership and Recognition</b></h2>
+                        
                         <br>
-                        <h2 class="featured-heading-text2" style="font-weight: 500;">Separation of Church and State</h2>
-                        <ul class="featured-heading-textp">
-                            <li style="padding-bottom: 15px;">There is secular education and there is religious education. Secular schools
-                                seek secular accreditation, while religious institutions receive sacred or
-                                ecclesiastic accreditation—each by their own peers.</li>
-                            <li style="padding-bottom: 15px;">Religious institutions need no secular accreditation because they offer no
-                                secular degrees. Religious schools like ours are, therefore, not legally
-                                required to become accredited by USDE.</li>
-                                
-                            <li>The US law of separation of Church and State also agrees with this. We are,
-                                therefore, not required by the law to seek accreditation from associations
-                                that are “recognized” by an agency of the federal government.</li>
-                        </ul>
-                        <br>
-                        <h2 class="featured-heading-text2" style="font-weight: 500;">Why not become accredited by the Department of Education?
-                        </h2>
-                        <p class="featured-heading-textp">Any school that is accredited by the government’s Department of Education is regulated by the
-                            government. The government therefore has a say in what can and cannot be taught by the school,
-                            among other things. These are often based on secular standards that seek to silence certain
-                            scriptural truths. Our standards are not secular and cannot be judged by secular standards!
+                       
+                        <p class="featured-heading-textp">Spirit Brood Theological Seminary is a <b> Certified Member of the Association of Independent
+                            Christian Colleges and Seminaries (AICCS).</b> AICCS is a faith-based, non-profit educational
+                                organization chartered in the State of Missouri. It is dedicated to the development,
+                                    strengthening, and accreditation of Christian schools of ministry that offer both in-classroom and
+                                        online programs.
                             
                             </p>
-                            <p class="featured-heading-textp">As we find in 1 Corinthians 2:14 “But the natural man receives not the things of the Spirit of God:
-                                for they are foolishness unto him: neither can he know them, because they are spiritually
-                                discerned.” Therefore a state agency is not able to determine if a spiritually directed learning
-                                institution is meeting the mandates of the Bible!
+                            <p class="featured-heading-textp">Through our membership with AICCS, over 1,000 institutions worldwide recognize and accept
+                                    credits and/or degrees issued by Spirit Brood Theological Seminary. Additionally, certain schools 
+                                    accredited by the U.S. Department of Education (USDE) may also accept our credentials.
+                                    However, please note that <b>credit transfers remain at the discretion of the receiving institution.</b>
                                 </p>
-                        <p class="featured-heading-textp">DOE Accreditation has many benefits for both educational institutions and students, but it is
-                            neither a guaranty nor is it the sole criterion of an excellent education. While Spirit Brood
-                            Theological Seminary is not accredited by an agency recognized by the U.S. Secretary of
-                            Education, our programs have been reviewed and certified by the Association of Independent
-                            Christian Colleges and Seminaries, based in Missouri USA. Further, we are duly registered as a
-                            Religious Education institution with Wyoming's Secretary of State.
+                        <p class="featured-heading-textp">To verify our standing with AICCS or to learn more about other institutions
+                             that recognize our credentials, please click here <a href="https://www.aiccs.org/updates" style="color: rgb(38, 119, 226); text-decoration: underline;" target="_blank">https://www.aiccs.org/updates</a>.
+
+
+
                             <br><br>
                             </p>
-                            <p class="featured-heading-textp">
-                            You may <a href="https://wyobiz.wyo.gov/Business/FilingDetails.aspx?eFNum=157210222087105034028133239113239011236067123069" style="color: rgb(38, 119, 226); text-decoration: underline;" target="_blank">click here</a> to view our status with the Secretary of State's office
-                        </p>
+                            
                     </div>
                   
                 </div>
