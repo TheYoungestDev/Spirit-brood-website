528a2a17e1df7d60221aa3977bbebf0579989ee1
Abdullahi Tijani <iamabdullahitijani@gmail.com>
2024-11-16 10:17:16 +0100

    Made some crucial updates to accreditations page

--- accreditations.php
+++ accreditations.php
@@ -5,9 +5,8 @@
          <section>
             <div class="featured-hero">
               <div class="img-content">
-                <h3>Certified Member</h3>
-                <span>Association of Independent Christian Colleges and Seminaries
-                    (AICCS)</span>.
+                <h3>Accreditation</h3>
+                <span>We are a religious exempt institution in Florida</span>.
               </div>
             </div>
         </section>
@@ -17,15 +16,15 @@
             <div class="container">
                 <div class="row">
                 <div class="col-md-12">
-                                            <!-- <h2 class="featured-heading-text1">Accreditation Page </h2>  -->
+                                            <h2 class="featured-heading-text1">Accreditation and Legal Status</h2> 
                                     
                         <!-- <h2 class="featured-heading-text1">Accreditation</h2><img src="/images/c/AICCS.png" alt="aciis" /> -->
                         <!-- <h3 class="featured-heading-text2">Certified Member of AICCS <br>
                             Association of Independent Christian Colleges and Seminaries</h3> -->
                         <p class="featured-heading-textp">Spirit Brood Theological Seminary operates under the legal framework provided by the State of
                             Florida, where we are duly incorporated as a Religious Institution. Florida law permits us to offer
-                                educational programs exclusively designed to prepare students for religious vocations, as
-                                  <b> ministers, professionals, or laypersons </b>  in the areas of ministry, counseling, theology, education,
+                                educational programs <b> exclusively designed to prepare students for religious vocations as
+                                   ministers, professionals, or laypersons </b>  in the areas of ministry, counseling, theology, education,
                                         administration, music, fine arts, media communications, and social work.                          
                         </p>
                         <p class="featured-heading-textp">We fully comply with the requirements of <b>Section 1005.06(1)(f), Florida Statutes,</b> and <b>Rule 6E5.001, Florida Administrative Code.</b> Consequently, we are not subject to the jurisdiction or
@@ -40,10 +39,10 @@
                             Theological Seminary has filed and submitted a notarized <b>Form 113RE</b> to the Commission for
                                 Independent Education as required.
                             </p>
-                            <p class="featured-heading-textp">If you are pursuing education at Spirit Brood Theological Seminary with the
+                            <!-- <p class="featured-heading-textp">If you are pursuing education at Spirit Brood Theological Seminary with the
                                         intent of obtaining employment or for some other reason, you should check
                                         with that organization BEFORE applying for enrollment. Spirit Brood
-                                        Theological Seminary assumes no liability of any kind.</p>
+                                        Theological Seminary assumes no liability of any kind.</p> -->
                         <br><br>
                         <h2 class="featured-heading-text2" style="font-weight: 500;"><b>Membership and Recognition</b></h2>
                         
@@ -62,7 +61,7 @@
                                     However, please note that <b>credit transfers remain at the discretion of the receiving institution.</b>
                                 </p>
                         <p class="featured-heading-textp">To verify our standing with AICCS or to learn more about other institutions
-                             that recognize our credentials, please click here <a href="https://www.aiccs.org/updates" style="color: rgb(38, 119, 226); text-decoration: underline;" target="_blank">https://www.aiccs.org/updates</a>.
+                             that recognize our credentials, please click <a href="https://www.aiccs.org/updates" style="color: rgb(38, 119, 226); text-decoration: underline;" target="_blank">here</a>.
 
 
 
